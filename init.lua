413b68439126d162f68cc111d714724b0872f252
Linxi Chen <linxichen88@gmail.com>
2024-07-08 03:36:34 -0400

    add bash lsp

--- init.lua
+++ init.lua
@@ -644,6 +644,7 @@ require('lazy').setup({
             return cwd
           end,
         },
+        bashls = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
